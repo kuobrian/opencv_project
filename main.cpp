8ca2adffd591dd05f96f705023e2065393f2afad
unknown <kuobrian614@gmail.com>
2021-10-14 21:11:21 +0800

    isContinuous function

--- main.cpp
+++ main.cpp
@@ -58,11 +58,20 @@ void salt(Mat img, int n) {
     }
 }
 
-void colorRestore(Mat img, int div=64) {
-    for(int j=0; j<img.rows; j++) {
-        uchar* data = img.ptr<uchar>(j);
-        for(int i=0; i<img.cols; i++) {
-            data[i] = data[i] / div*div + div/2;
+void colorRestore(const Mat &image, Mat &result, int div=64) {
+    
+    if(image.isContinuous()) {
+        image.reshape(1, 1);
+    }
+    int nc = image.cols * image.channels();
+    int nl = image.rows;
+
+
+    for(int j=0; j<nl; j++) {
+        const uchar* data_in= image.ptr<uchar>(j);
+        uchar* data_out= result.ptr<uchar>(j);
+        for(int i=0; i<nc; i++) {
+            data_out[i] = data_in[i] / div*div + div/2;
         }
     }
 }
@@ -74,10 +83,11 @@ int main()
     // facedetect();
     Mat img = imread("1.png");
     // salt(img, 3000);
-    colorRestore(img);
-
+    cv::Mat result;
+    result.create(img.rows, img.cols, img.type());
+    colorRestore(img, result);
     
-    cv::imshow("image",img);
+    cv::imshow("image",result);
     cv::waitKey();
 
     
